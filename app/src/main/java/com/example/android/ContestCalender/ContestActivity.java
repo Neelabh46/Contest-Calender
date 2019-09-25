4bd9a36ab0015965037d0c10ed66957168154fc5
Neelabh Gupta <neelabh78@gmail.com>
2019-09-25 23:31:55 +0530

    Use ViewModel and LiveData replacing Loaders

--- app/src/main/java/com/example/android/ContestCalender/ContestActivity.java
+++ app/src/main/java/com/example/android/ContestCalender/ContestActivity.java
@@ -45,7 +45,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.TimeZone;
 
-public class ContestActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<ArrayList<Contest>> {
+public class ContestActivity extends AppCompatActivity  {
 
     public static final String LOG_TAG = ContestActivity.class.getName();
     private TextView mEmptyView ;
@@ -62,95 +62,8 @@ public class ContestActivity extends AppCompatActivity implements LoaderManager.
         super.onCreate(savedInstanceState);
         setContentView(R.layout.contest_activity);
 
-        final RecyclerView contestRecyclerView = findViewById(R.id.recycler);
-        mEmptyView = findViewById(R.id.empty_view);
-        mContest = new ArrayList<>();
-        mAdapter = new ContestAdapters(mContest);        // Set the adapter on the {@link ListView}
-        contestRecyclerView.setAdapter(mAdapter);
-        mlinearlayoutmanager = new LinearLayoutManager(this);
-        contestRecyclerView.setLayoutManager(mlinearlayoutmanager);
-
-        ConnectivityManager cm =
-                (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
-
-        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
-        boolean isConnected = activeNetwork != null &&
-                activeNetwork.isConnectedOrConnecting();
-        if(isConnected)
-        {
-            LoaderManager loaderamanager = getLoaderManager();
-            loaderamanager.initLoader(1,null,this);
-
-        }else
-        {
-            ProgressBar bar = findViewById(R.id.loading_spinner);
-            bar.setVisibility(View.GONE);
-            mEmptyView.setText("You don't have any fucking internet connection");
-            mEmptyView.setVisibility(View.VISIBLE);
-        }
-        contestRecyclerView.addOnItemTouchListener(
-                new RecyclerItemClickListener(this, contestRecyclerView ,new RecyclerItemClickListener.OnItemClickListener() {
-                    @Override public void onItemClick(View view, int position) {
-                        Contest currentEarthquake = mAdapter.getItem(position);
-
-                        Uri earthquakeUri = Uri.parse(currentEarthquake.getMurl());
-
-                        Intent websiteIntent = new Intent(Intent.ACTION_VIEW, earthquakeUri);
-                        startActivity(websiteIntent);
-                        // do whatever
-                    }
-
-                    @Override
-                    public void onLongItemClick(View view, int position) {
-
-                    }
-                })
-        );
-    }
-    @Override
-    public Loader<ArrayList<Contest>> onCreateLoader(int i, Bundle bundle) {
-       SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
-
-        String orderBy = sharedPrefs.getString(
-                getString(R.string.settings_selection_key),
-                getString(R.string.settings_selection_default)
-        );
-
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        Date date = new Date();
-        formatter.setTimeZone(TimeZone.getTimeZone("GMT"));
-        String currenttime= formatter.format(date);
-        Uri baseUri = Uri.parse(url_string);
-        Uri.Builder uriBuilder = baseUri.buildUpon();
-        Log.e("time",currenttime);
-
-        uriBuilder.appendQueryParameter("resource__name", orderBy);
-        uriBuilder.appendQueryParameter("end__gte", currenttime);
-        uriBuilder.appendQueryParameter("order_by", "start");
-
-        return new ContestLoader(this, uriBuilder.toString(),orderBy);
     }
 
-    @Override
-    public void onLoadFinished(Loader<ArrayList<Contest>> loader, ArrayList<Contest> earthquakes) {
-        // Clear the adapter of previous earthquake data
-
-       // mEmptyView.setText(R.string.no_earthquakes);
-
-        ProgressBar bar = findViewById(R.id.loading_spinner);
-        bar.setVisibility(View.GONE);
-        if (earthquakes != null && !earthquakes.isEmpty()) {
-
-            mContest.clear();
-            mContest.addAll(earthquakes);
-            mAdapter.notifyDataSetChanged();
-        }
-    }
-    @Override
-    public void onLoaderReset(Loader<ArrayList<Contest>> loader) {
-        mContest.clear();
-        mAdapter.notifyDataSetChanged();
-    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main, menu);
